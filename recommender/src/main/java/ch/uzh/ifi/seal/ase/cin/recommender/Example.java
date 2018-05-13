2a3b0edd66cecb18d622ee9f236d7410677fce56
Nico Strebel <ncstrbl@gmail.com>
2018-05-13 18:55:17 +0200

    implement demonstration example

--- recommender/src/main/java/ch/uzh/ifi/seal/ase/cin/recommender/Example.java
+++ recommender/src/main/java/ch/uzh/ifi/seal/ase/cin/recommender/Example.java
@@ -1,11 +1,7 @@
 package ch.uzh.ifi.seal.ase.cin.recommender;
 
-import cc.kave.commons.model.events.IIDEEvent;
-import cc.kave.commons.model.events.completionevents.CompletionEvent;
-import cc.kave.commons.model.events.completionevents.Context;
 import cc.kave.commons.model.naming.codeelements.IMethodName;
-import cc.kave.commons.utils.io.IReadingArchive;
-import cc.kave.commons.utils.io.ReadingArchive;
+import cc.kave.commons.model.naming.impl.v0.codeelements.MethodName;
 import cc.kave.rsse.calls.datastructures.Tuple;
 import ch.uzh.ifi.seal.ase.cin.recommender.model.*;
 import ch.uzh.ifi.seal.ase.cin.recommender.persistence.ModelRepository;
@@ -14,78 +10,85 @@ import ch.uzh.ifi.seal.ase.cin.recommender.recommendation.BasicMethodRecommender
 import ch.uzh.ifi.seal.ase.cin.recommender.recommendation.MethodRecommender;
 import ch.uzh.ifi.seal.ase.cin.recommender.recommendation.RecommendationSystem;
 
-import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 import java.util.Set;
 
 public class Example {
 
-    private final static String eventsDir = "C:\\Users\\ncstr\\Downloads\\Events-170301\\2016-05-09";
+    public static final String MINED_MODEL_PATH = "C:\\Users\\ncstr\\MinedModel";
+    public static final String MODEL_PATH = "C:\\Users\\ncstr\\Model";
 
     public static void main(String[] args) {
-        Path modelPath = Paths.get("C:\\Users\\ncstr\\Model");
+        Query query = createQuery();
+        System.out.println("Query: ");
+        System.out.println(query);
+
+        demonstrateLocalIncrementation(query);
+        demonstrateMinedModel(query);
+    }
+
+    private static Query createQuery() {
+        String builder = "public void Method() {\n" +
+                "  string a = \"Foo\";\n" +
+                "  string b = a.\n" +
+                "}";
+        System.out.println("Analysed code: ");
+        System.out.println(builder);
+        System.out.println();
+        System.out.println();
+
+        Query q = new Query("System.String");
+        q.setEnclosingNodeKind(EnclosingNodeKind.DEFAULT);
+        String[] types = {"System.String"};
+        q.setExpectedTypes(types);
+        q.setEnclosingMethodVisibility(Visibility.PUBLIC);
+        q.setCompletionNodeKind(CompletionNodeKind.DEFAULT);
+        q.setEnclosingMethodKind(MethodKind.INSTANCE);
+        return q;
+    }
+
+    private static void demonstrateLocalIncrementation(Query query) {
+        Path modelPath = Paths.get(MODEL_PATH);
+        RecommendationSystem recommendationSystem = createRecommendationSystem(modelPath);
+
+        Set<Tuple<IMethodName, Double>> results = recommendationSystem.query(query);
+        System.out.println("=======Initial recommendation: ");
+        printResults(results);
+        System.out.println();
+        System.out.println();
+
+        System.out.print("Insert Selection: ");
+        Scanner in;
+        in = new Scanner(System.in);
+
+        String s = in.nextLine();
+        recommendationSystem.updateModel(query, new MethodName(s));
+        results = recommendationSystem.query(query);
+        System.out.println("=======New Recommendation: ");
+        printResults(results);
+        System.out.println();
+        System.out.println();
+    }
+
+    private static void printResults(Set<Tuple<IMethodName, Double>> results) {
+        results.forEach(System.out::println);
+    }
+
+    private static void demonstrateMinedModel(Query query) {
+        Path modelPath = Paths.get(MINED_MODEL_PATH);
+        RecommendationSystem recommendationSystem = createRecommendationSystem(modelPath);
+
+        Set<Tuple<IMethodName, Double>> results = recommendationSystem.query(query);
+        System.out.println("=======Globally trained Recommendation: ");
+        printResults(results);
+    }
+
+    private static RecommendationSystem createRecommendationSystem(Path modelPath) {
         ModelRepository repository = new OnDemandModelRepository(modelPath);
         MethodRecommender recommender = new BasicMethodRecommender();
 
-        RecommendationSystem recommendationSystem = new RecommendationSystem(repository, recommender);
-//        List<Context> contexts = processZips();
-//        contexts.forEach(c -> System.out.println(recommendationSystem.query(c)));
-        query(recommendationSystem);
-    }
-
-    private static void query(RecommendationSystem recommendationSystem) {
-        Query q = new Query("Test");
-        q.setEnclosingNodeKind(EnclosingNodeKind.WHILE);
-        String[] types = {"Test2"};
-        q.setExpectedTypes(types);
-        q.setEnclosingMethodVisibility(Visibility.PUBLIC);
-        q.setCompletionNodeKind(CompletionNodeKind.RETURN_VALUE);
-        q.setEnclosingMethodKind(MethodKind.INSTANCE);
-
-        Set<Tuple<IMethodName, Double>> results = recommendationSystem.query(q);
-        System.out.println("Results before: ");
-        System.out.println(results);
-
-//        recommendationSystem.updateModel(q, );
-        results = recommendationSystem.query(q);
-        System.out.println("Results after: ");
-        System.out.println(results);
-    }
-
-    private static List<Context> processZips() {
-        Set<String> userZips = FileUtils.findAllZips(eventsDir);
-        List<Context> contexts = new ArrayList<>();
-
-        for (String userZip : userZips) {
-            System.out.printf("\n#### processing user zip: %s #####\n", userZip);
-            contexts.addAll(processUserZip(userZip));
-        }
-
-        return contexts;
-    }
-
-    private static List<Context> processUserZip(String userZip) {
-        int numProcessedEvents = 0;
-        List<Context> contexts = new ArrayList<>();
-        // open the .zip file ...
-        try (IReadingArchive ra = new ReadingArchive(new File(eventsDir, userZip))) {
-            // ... and iterate over content.
-            // the iteration will stop after 200 events to speed things up, remove this
-            // guard to process all events.
-            while (ra.hasNext() && (numProcessedEvents++ < 20000)) {
-                /*
-                 * within the userZip, each stored event is contained as a single file that
-                 * contains the Json representation of a subclass of IDEEvent.
-                 */
-                IIDEEvent e = ra.getNext(IIDEEvent.class);
-                if (e instanceof CompletionEvent && ((CompletionEvent) e).context != null)
-                    contexts.add(((CompletionEvent) e).context);
-            }
-        }
-
-        return contexts;
+        return new RecommendationSystem(repository, recommender);
     }
 }
